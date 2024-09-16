7ec661431003caca327a3af0981735d38e11b162
salafiaji <salafiaji@gmail.com>
2024-09-16 13:00:45 +0100

    Created User entity with username, email, and password fields

--- src/main/java/com/azeez/quiz_assessment_api/demo/Test.java
+++
@@ -1,5 +0,0 @@
-package com.azeez.quiz_assessment_api.demo;
-
-public class Test {
-    // This class to be deleted
-}
---
+++ src/main/java/com/azeez/quiz_assessment_api/entity/User.java
@@ -0,0 +1,30 @@
+package com.azeez.quiz_assessment_api.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    private LocalDateTime modifiedAt;
+
+}
