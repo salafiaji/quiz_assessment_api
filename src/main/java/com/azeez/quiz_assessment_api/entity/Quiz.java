5fb62f1c6aefaa3ada81944fed0e2d13cd696c7c
salafiaji <salafiaji@gmail.com>
2024-09-22 13:15:54 +0100

    Implement Quiz and User Many-to-Many Relationship

---
+++ src/main/java/com/azeez/quiz_assessment_api/entity/Quiz.java
@@ -0,0 +1,40 @@
+package com.azeez.quiz_assessment_api.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "quizzes")
+public class Quiz {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "created_by")
+    private User createdBy;
+
+    @OneToMany(mappedBy = "quiz")
+    private List<QuizQuestion> questions;
+
+    @ManyToMany
+    @JoinTable(
+            name = "quiz_user",
+            joinColumns = @JoinColumn(name = "quiz_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    private List<User> users;
+}
--- src/main/java/com/azeez/quiz_assessment_api/entity/User.java
+++ src/main/java/com/azeez/quiz_assessment_api/entity/User.java
@@ -1,12 +1,8 @@
 package com.azeez.quiz_assessment_api.entity;
 
-import jakarta.persistence.Entity;
 import jakarta.persistence.*;
 import lombok.*;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
-
-import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @Setter
@@ -19,12 +15,16 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String username;
-    private String email;
-    private String password;
-    @CreationTimestamp
-    private LocalDateTime createdAt;
-    @UpdateTimestamp
-    private LocalDateTime modifiedAt;
 
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @ManyToMany(mappedBy = "users")
+    private List<Quiz> quizzes;
 }
