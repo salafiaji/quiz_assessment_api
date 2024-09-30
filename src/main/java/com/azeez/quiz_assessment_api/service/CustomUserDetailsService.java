b980a513bead831eedf873e7bdeb45e748d021f2
salafiaji <salafiaji@gmail.com>
2024-09-30 15:56:54 +0100

    Configure Spring Security to use JWT for authentication.

---
+++ src/main/java/com/azeez/quiz_assessment_api/service/CustomUserDetailsService.java
@@ -0,0 +1,21 @@
+package com.azeez.quiz_assessment_api.service;
+
+import com.azeez.quiz_assessment_api.entity.User;
+import com.azeez.quiz_assessment_api.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException(username + " not found"));
+        return user;
+    }
+}
