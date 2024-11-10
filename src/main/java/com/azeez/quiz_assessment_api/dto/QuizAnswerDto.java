5ebf575273266a3de03b38a11948654d8cfa3d87
salafiaji <salafiaji@gmail.com>
2024-11-10 11:17:34 +0100

    minor changes

---
+++ src/main/java/com/azeez/quiz_assessment_api/dto/QuizAnswerDto.java
@@ -0,0 +1,14 @@
+package com.azeez.quiz_assessment_api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class QuizAnswerDto {
+    private Long questionId;
+    private String userAnswer;
+}
+
